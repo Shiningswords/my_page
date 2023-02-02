ae8f57613256996a1034ed89bf0907ad5ef7d53c
GudochkinK <goodok-01@hotmail.com>
2023-02-02 15:21:35 +0300

    создание конвертера для обработки цифр в url приложения fruits

--- fruits/urls.py
+++ fruits/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<all_fruits>/', views.get_info_about_all_fruits)
+    path('<int:all_fruits>/', views.get_info_about_all_fruits_by_num),
+    path('<str:all_fruits>/', views.get_info_about_all_fruits)
+
 ]
--- fruits/views.py
+++ fruits/views.py
@@ -10,9 +10,13 @@ fruits = {
 }
 
 
-def get_info_about_all_fruits(request, all_fruits):
+def get_info_about_all_fruits(request, all_fruits: str):
     description = fruits.get(all_fruits, None)
     if description:
         return HttpResponse(description)
     else:
         return HttpResponseNotFound(f'Фрукт {all_fruits} не найден')
+
+
+def get_info_about_all_fruits_by_num(request, all_fruits: int):
+    return HttpResponse(f'Это номер {all_fruits}')
