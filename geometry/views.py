c74aed22eb0f0376ceb3bfc87f18ee2d455961e2
GudochkinK <goodok-01@hotmail.com>
2023-02-02 16:59:07 +0300

    создание обработчиков роутов приложения geometry

--- geometry/views.py
+++ geometry/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from math import pi
 
-# Create your views here.
+
+def get_rectangle_area(request, width: int, height: int):
+    return HttpResponse(f'Площадь прямоугольника размером {width}x{height} равна {width * height}')
+
+
+def get_square_area(request, width: int):
+    return HttpResponse(f'Площадь квадрата размером {width}x{width} равна {width * width}')
+
+
+def get_circle_area(request, radius: int):
+    return HttpResponse(f'Площадь круга радиусом {radius} равна {pi * radius ** 2}')
