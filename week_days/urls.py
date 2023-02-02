a3c0bc61500f7e51974c7b8666d9915067a5e4bd
GudochkinK <goodok-01@hotmail.com>
2023-02-02 16:00:24 +0300

    создание конвертера для обработки цифр в url приложения week_days

--- week_days/urls.py
+++ week_days/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<all_week>/', views.get_info_about_all_week)
+    path('<int:all_week>/', views.get_info_about_all_week)
 ]
--- week_days/views.py
+++ week_days/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+week = {
+    1: 'Понедельник',
+    2: 'Вторник',
+    3: 'Среда',
+    4: 'Четверг',
+    5: 'Пятница',
+    6: 'Суббота',
+    7: 'Воскресенье'
+}
 
-def get_info_about_all_week(request, all_week):
-    if all_week == 'monday':
-        return HttpResponse('Понедельник')
-    elif all_week == 'tuesday':
-        return HttpResponse('Вторник')
-    elif all_week == 'wednesday':
-        return HttpResponse('Среда')
-    elif all_week == 'thursday':
-        return HttpResponse('Четверг')
-    elif all_week == 'friday':
-        return HttpResponse('Пятница')
-    elif all_week == 'saturday':
-        return HttpResponse('Суббота')
-    elif all_week == 'sunday':
-        return HttpResponse('Воскресенье')
+
+def get_info_about_all_week(request, all_week: int):
+    description = week.get(all_week, None)
+    if description:
+        return HttpResponse(description)
     else:
         return HttpResponseNotFound(f'Такого дня недели не существует ({all_week})')
