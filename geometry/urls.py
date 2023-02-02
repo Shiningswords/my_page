d2251b67a3503026b258c95de727b9384c1a63f4
GudochkinK <goodok-01@hotmail.com>
2023-02-02 17:47:49 +0300

    создание перенаправление роутов по типу 'get_..._area' в приложении geometry

--- geometry/urls.py
+++ geometry/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('rectangle/<int:width>/<int:height>/', views.get_rectangle_area),
     path('square/<int:width>/', views.get_square_area),
-    path('circle/<int:radius>/', views.get_circle_area)
+    path('circle/<int:radius>/', views.get_circle_area),
+    path('get_rectangle_area/<int:width>/<int:height>/', views.get_rectangle_area_get),
+    path('get_square_area/<int:width>/', views.get_square_area_get),
+    path('get_circle_area/<int:radius>/', views.get_circle_area_get)
 ]
--- geometry/views.py
+++ geometry/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseRedirect
 from math import pi
 
 
@@ -13,3 +13,15 @@ def get_square_area(request, width: int):
 
 def get_circle_area(request, radius: int):
     return HttpResponse(f'Площадь круга радиусом {radius} равна {pi * radius ** 2}')
+
+
+def get_rectangle_area_get(request, width: int, height: int):
+    return HttpResponseRedirect(f'/calculate_geometry/rectangle/{width}/{height}/')
+
+
+def get_square_area_get(request, width: int):
+    return HttpResponseRedirect(f'/calculate_geometry/square/{width}/')
+
+
+def get_circle_area_get(request, radius: int):
+    return HttpResponseRedirect(f'/calculate_geometry/circle/{radius}/')
