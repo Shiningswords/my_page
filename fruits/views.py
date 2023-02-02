2d3d6b1c4f233efe8c896f0c55f7ba6ce413f934
GudochkinK <goodok-01@hotmail.com>
2023-02-02 15:08:05 +0300

    Рефакторинг функции get_info_about_all_fruits

--- fruits/views.py
+++ fruits/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+fruits = {
+    'apple': 'Яблоко',
+    'banana': 'Банан',
+    'ananas': 'Ананас',
+    'lemon': 'Лимон',
+    'mango': 'Манго'
+}
+
 
 def get_info_about_all_fruits(request, all_fruits):
-    if all_fruits == 'apple':
-        return HttpResponse('Яблоко')
-    elif all_fruits == 'banana':
-        return HttpResponse('Банан')
+    description = fruits.get(all_fruits, None)
+    if description:
+        return HttpResponse(description)
     else:
         return HttpResponseNotFound(f'Фрукт {all_fruits} не найден')
