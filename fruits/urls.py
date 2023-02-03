52228d322741191fb5a10dbfd962801fa4a9c8e3
GudochkinK <goodok-01@hotmail.com>
2023-02-03 17:19:15 +0300

    Создание дополнительного роута type и его обработка (по типам фрукты и ягоды)

--- fruits/urls.py
+++ fruits/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('type/', views.types),
+    path('type/<str:type_fruits>/', views.types_f, name='type_f'),
     path('<int:all_fruits>/', views.get_info_about_all_fruits_by_num),
     path('<str:all_fruits>/', views.get_info_about_all_fruits, name='fruits-name')
 
--- fruits/views.py
+++ fruits/views.py
@@ -7,7 +7,15 @@ fruits = {
     'banana': 'Банан',
     'ananas': 'Ананас',
     'lemon': 'Лимон',
-    'mango': 'Манго'
+    'mango': 'Манго',
+    'strawberry': 'Клубника',
+    'cherry': 'Вишня',
+    'raspberry': 'Малина'
+}
+
+fruits_type = {
+    'fruits': ['apple', 'banana', 'ananas', 'lemon', 'mango'],
+    'berry': ['strawberry', 'cherry', 'raspberry']
 }
 
 
@@ -16,6 +24,24 @@ def index(request):
     for fruit in fruits:
         redirect_path = reverse('fruits-name', args=(fruit,))
         li_elements += '<li>' + f'<a href={redirect_path}>' + fruit.title() + '</a>' + '</li>'
+    ol_elements = '<ol>' + li_elements + '</ol>'
+    return HttpResponse(ol_elements)
+
+
+def types(request):
+    li_elements = ''
+    for t in fruits_type:
+        redirect_path = reverse('type_f', args=(t,))
+        li_elements += '<li>' + f'<a href={redirect_path}>' + t.title() + '</a>' + '</li>'
+    ol_elements = '<ol>' + li_elements + '</ol>'
+    return HttpResponse(ol_elements)
+
+
+def types_f(request, type_fruits):
+    li_elements = ''
+    for t in fruits_type[type_fruits]:
+        redirect_path = reverse('fruits-name', args=(t,))
+        li_elements += '<li>' + f'<a href={redirect_path}>' + t.title() + '</a>' + '</li>'
     ol_elements = '<ol>' + li_elements + '</ol>'
     return HttpResponse(ol_elements)
 
