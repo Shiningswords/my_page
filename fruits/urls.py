7af115e1bd8775c98bee1c14d005d8a5597a387e
GudochkinK <goodok-01@hotmail.com>
2023-02-06 17:41:39 +0300

    применение тега url

--- fruits/urls.py
+++ fruits/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='fruits-index'),
     path('type/', views.types),
     path('type/<str:type_fruits>/', views.types_f, name='type_f'),
     path('<int:all_fruits>/', views.get_info_about_all_fruits_by_num),
